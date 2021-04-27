dfff7835cdf1cdf291583927f934543ce5807007
kylefarmer85 <kylefarmer85@gmail.com>
2021-04-27 17:15:09 -0500

    algo practice queue, trees BF DF

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,52 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+  add(data) {
+    this.children.push(new Node(data));
+  }
+  remove(data) {
+    this.children = this.children.filter(node => node.data !== data);
+  }
+}
 
-class Tree {}
+class Tree {
+  constructor () {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+    while(arr.length) {
+      const node = arr.shift();
+      arr.push(...node.children);
+      fn(node);
+    }
+  }
+
+  traverseDF(fn) {
+    const arr = [this.root];
+    while(arr.length) {
+      const node = arr.shift();
+      arr.unshift(...node.children);
+      fn(node);
+    }
+  }
+}
+
+  // fn is a callback function which take a node as an argument.
+  // You can see the below code in "test.js" where traverseBF is called with a callback function.
+  // (here callback function is passed while testing).
+
+  //  In test.js
+ 
+  //   t.traverseBF(node => {
+  //     letters.push(node.data);
+  //   });
+
 
 module.exports = { Tree, Node };
