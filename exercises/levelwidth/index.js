6950793bd35ec7dfc2a5d3b97a248ff95bedb03e
kylefarmer85 <kylefarmer85@gmail.com>
2021-04-28 18:23:18 -0500

    binary search tree practice

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let counters = [0];
+  const arr = [root, 's'];
+
+  while (arr.length > 1) {
+    const node = arr.shift();
+    if (node === 's') {
+      arr.push(node);
+      counters.push(0);
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+}
 
 module.exports = levelWidth;
